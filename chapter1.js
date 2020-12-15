613f7a2e4dcce755b3b5ecb77a51833d3569cfbd
Ron Gerlach <wordsmith4u@hotmail.com>
2020-12-14 20:23:49 -0500

    New Commits

---
+++ chapter1.js
@@ -0,0 +1,11 @@
+console.log("Agnes" || "Louise");
+console.log("Agnes" == true);
+
+let ten = 10;
+console.log(ten * ten);
+
+let mood = "light";
+console.log(mood);
+
+mood = "dark";
+console.log(mood);
